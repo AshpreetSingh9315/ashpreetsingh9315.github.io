711d69c60a96a9d3308be7f8539b0da6826c86b6
AshpreetSingh9315 <ashpreetsingh9212@gmail.com>
2023-05-03 13:13:39 +0530

    portfolio

---
+++ Ash.js
@@ -0,0 +1,15 @@
+var loading = document.getElementById('loading');
+
+var abc = ()=>{
+    loading.style.display='none'
+}
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
